82d327200be61a40bf5e106acee7f8dee1d41238
Miraculixx <miraculixxyt@gmail.com>
2024-02-26 02:21:51 +0100

    Add GuildKey extension

--- src/main/kotlin/de/miraculixx/mlog/discord/GuildKey.kt
+++ src/main/kotlin/de/miraculixx/mlog/discord/GuildKey.kt
@@ -1,6 +1,7 @@
 package de.miraculixx.mlog.discord
 
 import kotlinx.serialization.Serializable
+import net.dv8tion.jda.api.entities.Guild
 import kotlin.time.Duration
 
 @Serializable
@@ -8,6 +9,8 @@ data class GuildKey(
     val guildId: String,
     val modId: String
 )
+
+fun Guild.toKey(modId: String) = GuildKey(id, modId)
 
 @Serializable
 data class GuildConfiguration(
