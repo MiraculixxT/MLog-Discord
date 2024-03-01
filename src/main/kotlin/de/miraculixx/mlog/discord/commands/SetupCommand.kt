89d210ea4c0b7a40cb780f2304bf2dd56a8be8b2
Miraculixx <miraculixxyt@gmail.com>
2024-03-01 01:10:58 +0100

    Add setup command

--- src/main/kotlin/de/miraculixx/mlog/discord/commands/SetupCommand.kt
+++ src/main/kotlin/de/miraculixx/mlog/discord/commands/SetupCommand.kt
@@ -1,11 +1,141 @@
 package de.miraculixx.mlog.discord.commands
 
+import de.miraculixx.mlog.discord.*
+import de.miraculixx.mlog.sql.SQL
 import de.miraculixx.mlog.utils.entities.SlashCommandEvent
+import dev.minn.jda.ktx.messages.Embed
+import dev.minn.jda.ktx.messages.reply_
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import kotlin.time.Duration
 
-class SetupCommand: SlashCommandEvent {
+class SetupCommand : SlashCommandEvent {
     override suspend fun trigger(it: SlashCommandInteractionEvent) {
-        TODO("Not yet implemented")
+        val guild = it.guild ?: return
+        if (!GuildData.checkGuildPresence(guild.idLong, guild.name)) {
+            it.databaseError(100)
+            return
+        }
+
+        when (it.subcommandName) {
+            "register-mod" -> {
+                val modID = it.getOption("project")?.asString ?: return
+                val typeID = it.getOption("type")?.asInt ?: return
+                val timeout = it.getOption("timeout")?.asString ?: return
+
+                val rows = SQL.call("SELECT ModID FROM Projects JOIN Guilds ON Guilds.GKey = Projects.GKey WHERE ID=?") {
+                    setLong(1, guild.idLong)
+                }.fetchSize
+                val guildMetrics = GuildData.getGuildMetrics(guild.idLong)
+                if (guildMetrics == null) {
+                    it.databaseError(101)
+                    return
+                }
+
+                if (rows >= guildMetrics.maxProjects) {
+                    it.reply_(embeds = listOf(Embed {
+                        title = "<:no:1212465201509568543>  || Project Not Added"
+                        description = "The project `mod` could not be added. Your guild already reached the maximum of **3** concurrent projects!\n" +
+                                "\n" +
+                                "We currently have limited resources, the limit might expand in the future for supporter. You can [contact us](https://dc.mutils.net) via ticket for manual expanding if required."
+                        colorError()
+                        mlogFooter()
+                    })).queue()
+                }
+
+                val type = GuildProjectType.fromID(typeID)
+                val timeoutDuration = Duration.parseOrNull(timeout)
+                if (timeoutDuration == null) {
+                    it.durationError()
+                    return
+                }
+
+                if (!GuildData.registerNewProject(guild.toKey(modID), timeoutDuration, type)) {
+                    it.databaseError(102)
+                    return
+                }
+
+                it.reply_(embeds = listOf(Embed {
+                    title = "<:yes:1212465222175031316>  || Project Added"
+                    description = "The project `$modID` has been added to your guild!\n" +
+                            "You can now generate a code for your users to request their logs.\n" +
+                            colorSuccess()
+                    mlogFooter()
+                }))
+            }
+
+            "unregister-mod" -> {
+                val modID = it.getOption("project")?.asString ?: return
+                if (!GuildData.removeProject(guild.toKey(modID))) {
+                    it.noProjectError(modID)
+                    return
+                }
+
+                it.reply_(embeds = listOf(Embed {
+                    title = "<:yes:1212465222175031316>  || Project Removed"
+                    description = "The project `$modID` has been removed from your guild!\n" +
+                            "You can no longer generate a code for your users to request their logs and all current codes are removed."
+                    colorSuccess()
+                    mlogFooter()
+                })).queue()
+            }
+
+            "change-timeout" -> {
+                val modID = it.getOption("project")?.asString ?: return
+                val timeout = it.getOption("timeout")?.asString ?: return
+
+                val timeoutDuration = Duration.parseOrNull(timeout)
+                if (timeoutDuration == null) {
+                    it.durationError()
+                    return
+                }
+
+                val rows = SQL.update("UPDATE Projects SET Timeout=? WHERE GKey=(SELECT GKey FROM Guilds WHERE ID=?) AND ModID=?") {
+                    setString(1, timeoutDuration.toIsoString())
+                    setLong(2, guild.idLong)
+                    setString(3, modID)
+                }
+                when {
+                    rows == 0 -> it.noProjectError(modID)
+                    rows > 0 -> {
+                        it.reply_(embeds = listOf(Embed {
+                            title = "<:yes:1212465222175031316>  || Timeout Changed"
+                            description = "The timeout for the project `$modID` has been changed to `$timeoutDuration`!"
+                            colorSuccess()
+                            mlogFooter()
+                        })).queue()
+                    }
+
+                    else -> it.databaseError(104)
+                }
+            }
+        }
     }
 
+    private fun SlashCommandInteractionEvent.databaseError(code: Int) {
+        reply_(embeds = listOf(Embed {
+            title = "<:no:1212465201509568543>  || Something Went Wrong"
+            description = "Oops, that shouldn't have happened.\nPlease try again later or [contact us](https://dc.mutils.net) if the issue persists.\n" +
+                    "Error Position: `$code`"
+            colorError()
+            mlogFooter()
+        })).queue()
+    }
+
+    private fun SlashCommandInteractionEvent.noProjectError(modID: String) {
+        reply_(embeds = listOf(Embed {
+            title = "<:no:1212465201509568543>  || Project Not Removed"
+            description = "The project `$modID` could not be removed.\nIt is not configured for this guild!"
+            colorError()
+            mlogFooter()
+        })).queue()
+    }
+
+    private fun SlashCommandInteractionEvent.durationError() {
+        reply_(embeds = listOf(Embed {
+            title = "<:no:1212465201509568543>  || Invalid Timeout Duration"
+            description = "The timeout provided is invalid.\nPlease provide a valid duration in the format of `1h`, `30m`, `1h30m`, `1.5d` etc."
+            colorError()
+            mlogFooter()
+        })).queue()
+    }
 }
--- src/main/kotlin/de/miraculixx/mlog/utils/manager/SlashCommandManager.kt
+++ src/main/kotlin/de/miraculixx/mlog/utils/manager/SlashCommandManager.kt
@@ -1,30 +1,30 @@
 package de.miraculixx.mlog.utils.manager
 
+import de.miraculixx.mlog.LOGGER
 import de.miraculixx.mlog.MLogBot
 import de.miraculixx.mlog.discord.commands.CodeCommand
+import de.miraculixx.mlog.discord.commands.ManageCommand
 import de.miraculixx.mlog.discord.commands.SetupCommand
 import dev.minn.jda.ktx.events.listener
-import dev.minn.jda.ktx.interactions.commands.option
-import dev.minn.jda.ktx.interactions.commands.slash
-import dev.minn.jda.ktx.interactions.commands.subcommand
-import dev.minn.jda.ktx.interactions.commands.updateCommands
+import dev.minn.jda.ktx.interactions.commands.*
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel
 import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions
-import net.dv8tion.jda.api.interactions.commands.OptionType
 
 object SlashCommandManager {
     private val commands = mapOf(
         "setup" to SetupCommand(),
-        "code" to CodeCommand()
-        )
+        "code" to CodeCommand(),
+        "manage" to ManageCommand()
+    )
 
     fun startListen(jda: JDA) {
         jda.listener<SlashCommandInteractionEvent> {
             val commandClass = commands[it.name] ?: return@listener
-            println("${it.user.name}: /${it.name} ${it.subcommandName}")
+            val subCommandGroup = it.subcommandGroup
+            LOGGER.info("${it.user.name}: /${it.name}${if (subCommandGroup == null) "" else " $subCommandGroup"} ${it.subcommandName}")
             commandClass.trigger(it)
         }
         jda.listener<CommandAutoCompleteInteractionEvent> {
@@ -38,20 +38,41 @@ object SlashCommandManager {
             slash("setup", "Setup MLog for your Server") {
                 defaultPermissions = DefaultMemberPermissions.DISABLED
                 subcommand("register-mod", "Register a new mod for your server") {
-                    option<String>("mod-id", "The ID of your mod/plugin", true)
+                    option<String>("project", "The ID of your mod/plugin", true)
+                    option<String>("type", "Client side, server side or both?", true, true) {
+                        choice("client", 0)
+                        choice("server", 0)
+                        choice("both", 0)
+                    }
+                    option<String>("timeout", "The time after each code becomes unusable (e.g. 1d 6h)", true)
                 }
                 subcommand("unregister-mod", "Unregister a mod from your server") {
-                    addOption(OptionType.STRING, "mod-id", "The ID of your mod/plugin", true)
-                    option<String>("mod-id", "The ID of your mod/plugin", true)
+                    option<String>("project", "The ID of your mod/plugin", true, true)
                 }
                 subcommand("code-timeout", "Set the timeout for new generated codes") {
-                    option<String>("timeout", "1d | 6h 30m 10s | 1.5d | ...", true)
+                    option<String>("project", "The ID of your mod/plugin", true, true)
+                    option<String>("timeout", "The time after each code becomes unusable (e.g. 1d 6h)", true)
                 }
+            }
+
+            slash("manage", "Manage your MLog settings") {
+                defaultPermissions = DefaultMemberPermissions.DISABLED
+                subcommand("info", "Get information about your server")
+                subcommand("projects", "List all registered projects for your server")
+                addSubcommandGroups(SubcommandGroup("codes", "Manage your codes for your server") {
+                    subcommand("list", "List all active codes for a project") {
+                        option<String>("project", "The ID of your mod/plugin", true, true)
+                    }
+                    subcommand("remove", "Remove a code from a project") {
+                        option<String>("project", "The ID of your mod/plugin", true, true)
+                        option<String>("code", "The code to remove", true, true)
+                    }
+                })
             }
 
             slash("code", "Get a new code to register a new mod") {
                 defaultPermissions = DefaultMemberPermissions.DISABLED
-                option<String>("mod", "The target mod for receiving logs", true, true)
+                option<String>("project", "The target project for receiving logs", true, true)
                 option<MessageChannel>("channel", "The channel to send the logs to", false)
                 option<Boolean>("announce", "Announce generated code public in current channel?", false)
             }
